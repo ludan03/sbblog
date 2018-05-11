75cdb71c155b52a4e9d1bb3753bff75bd67424f4
ludan03 <446354406@qq.com>
2018-05-11 23:38:09 +0800

    new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/link/xc/sbblog/SbblogApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/link/xc/sbblog/SbblogApplicationTests.java

---
+++ src/main/java/link/xc/sbblog/SbblogApplication.java
@@ -0,0 +1,12 @@
+package link.xc.sbblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbblogApplication.class, args);
+	}
+}
